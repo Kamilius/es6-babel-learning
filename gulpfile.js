15adf62fb8de9172c94792df57f656fa42d64b39
Kamilius <kamiklviv@gmail.com>
2015-04-14 14:02:29 +0300

    Gulp configured

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,21 @@ var gulp = require('gulp'),
 
 gulp.task('default', function() {
   gulp.src('src/app/**/*.js')
-    .pipe(watch('src/**/*.js'))
+    .pipe(watch('src/**/*.js'), { verbose: true })
     .pipe(concat('script.js'))
     .pipe(babel())
     .pipe(gulp.dest('build/'));
 
-  gulp.src('src/sass/*.sass')
-    .pipe(watch('src/sass/*.sass'))
+  gulp.src('src/content/sass/*.sass')
+    .pipe(watch('src/content/sass/*.sass'), { verbose: true })
     .pipe(sass())
     .pipe(gulp.dest('build/'));
 
   gulp.src('src/index.html')
-    .pipe(watch('src/index.html'))
+    .pipe(watch('src/index.html'), { verbose: true })
     .pipe(gulp.dest('build/'));
 
   gulp.src('src/partials/*.html')
-    .pipe(watch('src/partials/*.html'))
+    .pipe(watch('src/partials/*.html'), { verbose: true })
     .pipe(gulp.dest('build/partials/'));
 });
