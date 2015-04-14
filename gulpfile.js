c46096750c566b5ee98e1e29054cc1f20d9c526a
Kamilius <kamiklviv@gmail.com>
2015-04-14 14:52:00 +0300

    Gulp config 100% functional

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,50 @@ var gulp = require('gulp'),
     copy = require('gulp-copy'),
     watch = require('gulp-watch');
 
+var sources = {
+  babel: ['src/app/*.js', 'src/app/**/*.js'],
+  sass: 'src/content/sass/*.sass',
+  partials: ['src/index.html', 'src/partials/*.html']
+}
+
 
 gulp.task('default', function() {
-  gulp.src('src/app/**/*.js')
-    .pipe(watch('src/**/*.js'), { verbose: true })
+  gulp.start('babel');
+  gulp.start('sass');
+  gulp.start('partials');
+  gulp.src('src/scripts/*.js')
+    .pipe(gulp.dest('build/scripts/'));
+
+  watch(sources.babel, function() {
+    gulp.start('babel');
+  });
+
+  watch(sources.sass, function() {
+    gulp.start('sass');
+  });
+
+  watch(sources.partials, function() {
+    gulp.start('partials');
+  });
+});
+
+gulp.task('babel', function() {
+  gulp.src(sources.babel)
     .pipe(concat('script.js'))
     .pipe(babel())
     .pipe(gulp.dest('build/'));
+});
 
-  gulp.src('src/content/sass/*.sass')
-    .pipe(watch('src/content/sass/*.sass'), { verbose: true })
-    .pipe(sass())
+gulp.task('sass', function() {
+  gulp.src(sources.sass)
+    .pipe(sass({ indentedSyntax: true }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('partials', function() {
+  gulp.src(sources.partials[0])
     .pipe(gulp.dest('build/'));
 
-  gulp.src('src/index.html')
-    .pipe(watch('src/index.html'), { verbose: true })
-    .pipe(gulp.dest('build/'));
-
-  gulp.src('src/partials/*.html')
-    .pipe(watch('src/partials/*.html'), { verbose: true })
+  gulp.src(sources.partials[1])
     .pipe(gulp.dest('build/partials/'));
 });
